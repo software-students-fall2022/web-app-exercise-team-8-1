96876479271be1088bfd882bee58a64bbd12db82
fnassar <fan6236@nyu.edu>
2022-10-19 22:47:33 -0400

    css

---
+++ static/js/list.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    let burgerFlag = false;
+    if (!burgerFlag) {
+        document.getElementById("burger").addEventListener("click", function() {
+            console.log("burger clicked");
+            document.getElementById("filter_list").style.display = "block";
+        })
+        burgerFlag = true;
+    } else {
+        document.getElementById("burger").addEventListener("click", function() {
+            console.log("burger clicked");
+            document.getElementById("filter_list").style.display = "none";
+        })
+        burgerFlag = false;
+    }
+}
